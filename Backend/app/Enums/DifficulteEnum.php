9caaa910514ae5ea56c501af9c7b71ddb1e8bcb5
Traore Allassane <traoreallassane2255@gmail.com>
2026-02-07 10:33:22 +0000

    Mise en place des migrations

---
+++ Backend/app/Enums/DifficulteEnum.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enums;
+
+enum DifficulteEnum: string
+{
+    case FACILE = "Facile";
+    case NORMAL = "Normal";
+    case DIFFICILE = "Difficile";
+}
---
+++ Backend/database/migrations/2026_02_07_101047_create_taches_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Enums\DifficulteEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('taches', function (Blueprint $table) {
+            $table->id();
+            $table->string('titre');
+            $table->mediumText('description');
+            $table->enum('difficulte', DifficulteEnum::cases());
+            $table->date('date_debut');
+            $table->date('date_fin');
+
+            $table->foreignId('equipe_id')->constrained();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('taches');
+    }
+};
