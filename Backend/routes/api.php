f44a1aa9a7b29dc8d6cd7cd72e9cb8404d8d6b12
Traore Allassane <traoreallassane2255@gmail.com>
2026-02-15 11:10:31 +0000

    Backend : Liste des membres de l'equipe

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\V1\admin\AdminMenmbreController;
 use App\Http\Controllers\Api\V1\admin\AdminTacheController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,12 +17,17 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::prefix('v1')->group(function () {
         // Gestion des taches par ADMIN
         Route::middleware("admin")->prefix('admin')->group(function () {
+            // gestion des taches
             Route::controller(AdminTacheController::class)->group(function () {
                 Route::get('/taches', 'index');
                 Route::get('/taches/{tache}', 'show');
                 Route::post('/taches', "store");
                 Route::put('/taches/{tache}', "update");
                 Route::delete('/taches/{tache}', 'destroy');
+            });
+
+            Route::controller(AdminMenmbreController::class)->group(function() {
+                Route::get('/equipe', 'index');
             });
         });
     });
